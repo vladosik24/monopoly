8d6b9d4cefa54cc26d25b64a01fe4161f34564ca
vladosik24 <kuriliak.official@gmail.com>
2026-04-21 06:23:05 +0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'monopoly-v1';
+const FILES = [
+  '/monopoly/',
+  '/monopoly/index.html',
+  '/monopoly/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
